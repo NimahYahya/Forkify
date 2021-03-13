7ee3e8cb6cc2a0b068ca34151b84fc314b9e01c2
Nimah Yahya <nimahyahya@outlook.sa>
2021-03-13 06:48:42 +0300

    check for empty search

--- src/js/model.js
+++ src/js/model.js
@@ -44,6 +44,7 @@ export const loadRecipe = async function (id) {
 
 export const loadSearchResults = async function (query) {
   try {
+    if (query === '') return;
     state.search.query = query;
     const data = await AJAX(`${API_URL}?search=${query}&key=${KEY}`);
     console.log(data);
